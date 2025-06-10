a917b879cb7835c0008c8d387e67b110f496afa3
Jens Zech <j.zech@thalia.de>
2025-06-10 07:28:16 +0200

    Pipeline test

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,4 +1,6 @@
 
 import packageJson from "./package.json";
 export const APP_VERSION = packageJson.version || "0.0.1";
+
+//TODO Move to .env file
 export const SUPPORT_EMAIL = 'j.zech@thalia.com';
