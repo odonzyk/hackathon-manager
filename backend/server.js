82e602844c71f50e8d672d38b2e6bdf1b821c577
Jens Zech <j.zech@thalia.de>
2025-06-13 14:57:46 +0200

    Restart email fixed

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const participateRoutes = require('./src/routes/participant');
 const initiatorRoutes = require('./src/routes/initiator');
 const adminRoutes = require('./src/routes/admin');
 const { dbInitialisation } = require('./src/utils/db/db');
-const { sendActivationEmail } = require('./src/utils/emailUtils');
+const { sendActivationEmail, sendServerRestartNotification } = require('./src/utils/emailUtils');
 
 // CORS-Options for Frontend
 const corsOptions = {
@@ -85,8 +85,8 @@ function sendTestmail() {
       name: 'Emil Mustermann',
       activation_code: Math.random().toString(36).substring(2, 15)
     };
-    sendActivationEmail(newUser).catch((err) => {
-      logger.error(`Error sending activation email for user ${newUser.email}: ${err.message}`);
+    sendServerRestartNotification(newUser.email).catch((err) => {
+      logger.error(`Error sending server restart notification for user ${newUser.email}: ${err.message}`);
     });
 }
 // Prometheus integration
