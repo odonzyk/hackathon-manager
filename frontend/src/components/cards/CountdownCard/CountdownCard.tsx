0a9ae55510bd6fea4dda89b16a160fb9e9881749
Jens Zech <j.zech@thalia.de>
2025-06-26 10:44:30 +0200

    kleines Update

--- frontend/src/components/cards/CountdownCard/CountdownCard.tsx
+++ frontend/src/components/cards/CountdownCard/CountdownCard.tsx
@@ -8,33 +8,58 @@ interface CountdownCardProps {
   event: Event | null;
 }
 
+const EventState = {
+  upcoming: 1,
+  ongoing: 2,
+  ended: 3,
+};
+
 const CountdownCard: React.FC<CountdownCardProps> = ({ event }) => {
+  console.log('Start Time, End Time:', event?.start_time, event?.end_time);
+
+  if (!event || !event.start_time || !event.end_time) {
+    console.warn('Event data is not available or incomplete:', event);
+    return null; // Return null if event data is not available
+  }
+  let eventState = EventState.ended;
+  const currentTime = Math.floor(new Date().getTime() / 1000);
+  if (event?.start_time <= currentTime && event?.end_time > currentTime) {
+    eventState = EventState.ongoing;
+  } else if (event?.start_time > currentTime) {
+    eventState = EventState.upcoming;
+  } else {
+    eventState = EventState.ended;
+  }
+  console.log('Event State:', eventState);
+
   return (
     <>
-      {event?.start_time && event?.end_time ? (
-        <IonCard className="hackathon-card no-hover ">
-          {event.end_time < Math.floor(new Date().getTime() / 1000) ? (
-            <>
-              <IonCardHeader>
-                <IonCardTitle>Hackathon bereits vorbei</IonCardTitle>
-              </IonCardHeader>
-              <IonCardContent>
-                seit{' '}
-                {formatCountdown(-(event.start_time - Math.floor(new Date().getTime() / 1000)))}
-              </IonCardContent>
-            </>
-          ) : (
-            <>
-              <IonCardHeader>
-                <IonCardTitle>Hackathon Countdown</IonCardTitle>
-              </IonCardHeader>
-              <IonCardContent>
-                {formatCountdown(event.start_time - Math.floor(new Date().getTime() / 1000))}
-              </IonCardContent>
-            </>
-          )}
+      {eventState === EventState.ended && (
+        <IonCard className="hackathon-card no-hover">
+          <IonCardHeader>
+            <IonCardTitle>Hackathon bereits vorbei</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>seit {formatCountdown(currentTime - event.end_time)}</IonCardContent>
         </IonCard>
-      ) : null}
+      )}
+
+      {eventState === EventState.upcoming && (
+        <IonCard className="hackathon-card no-hover">
+          <IonCardHeader>
+            <IonCardTitle>Hackathon Countdown</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>{formatCountdown(event.start_time - currentTime)}</IonCardContent>
+        </IonCard>
+      )}
+
+      {eventState === EventState.ongoing && (
+        <IonCard className="hackathon-card no-hover">
+          <IonCardHeader>
+            <IonCardTitle>Hackathon läuft, bis zur Demo noch</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>{formatCountdown(event.end_time - currentTime)}</IonCardContent>
+        </IonCard>
+      )}
     </>
   );
 };
