0329bfb0cc8054d450fed6553515480451258d23
Jens Zech <j.zech@thalia.de>
2025-07-03 09:55:20 +0200

    cleanup project

---
+++ backend/volumes/Projects.example.js
@@ -0,0 +1,27 @@
+const defaultProjects = [
+  {
+    event_id: 1,
+    id: 1,
+    status_id: 3,
+    idea: 'Example Project 1',
+    description: 'Dies ist ein Beispielprojekt, um die Funktionalität zu demonstrieren.',
+    team_name: 'Team Innovators',
+    team_avatar: '/assets/avatars/avatar_1.png',
+    goal: 'Einführung eines neuen Features zur Verbesserung der Nutzererfahrung.',
+    components: 'Meetingraum, Whiteboard, Beamer',
+    skills: 'Offen für alle, besonders IT, UX/UI Design, Projektmanagement'
+  },
+  {
+    event_id: 1,
+    id: 2,
+    status_id: 3,
+    idea: 'Example Project 2',
+    description: 'Dies ist ein weiteres Beispielprojekt, um die Funktionalität zu demonstrieren.',
+    team_name: 'Team Visionaries',
+    team_avatar: '/assets/avatars/avatar_1.png',
+    goal: 'Entwicklung einer innovativen Lösung zur Optimierung von Arbeitsabläufen.',
+    components: 'Meetingraum, Whiteboard, Beamer',
+    skills: 'Offen für alle, besonders IT, UX/UI Design, Projektmanagement'
+  }];
+
+module.exports = { defaultProjects };
